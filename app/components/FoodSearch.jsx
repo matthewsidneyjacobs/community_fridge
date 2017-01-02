d1535b0f95bde4b76090e9dd17866e77924be5e0
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2017-01-02 16:21:16 -0700

    added search function NOT WIRED UP YET

---
+++ app/components/FoodSearch.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+
+var FoodSearch = React.createClass({
+  handleSearch: function() {
+    var searchText = this.refs.searchText.value;
+
+    this.props.onSearch(searchText);
+  },
+  render: function () {
+    return (
+      <div>
+        <div>
+          <input type="search" ref="searchText" placeholder="Search Food Items" onChange={this.handleSearch}/>
+          {/* //onchange kinda seems like filter in angular */}
+        </div>
+        {/* will i need to show completed trades? maybe past trades? this is the label for it */}
+        {/* <div>
+          <label>
+            <input type="checkbox" ref="showCompleted" onChange={this.handleSearch}/>
+            Show Completed Trades
+          </label>
+        </div> */}
+      </div>
+    )
+  }
+})
+
+module.exports = FoodSearch;
--- app/components/PersonalItems.jsx
+++ app/components/PersonalItems.jsx
@@ -1,10 +1,12 @@
 var React = require('react');
 var FoodList = require('FoodList');
 var PersonalControls = require('PersonalControls');
+var FoodSearch = require('FoodSearch');
 
 var PersonalItems = React.createClass({
   getInitialState: function() {
     return {
+      searchText: '',
       items: [
         {
           id: 1,
@@ -28,10 +30,16 @@ var PersonalItems = React.createClass({
   handleAddItem: function(text) {
     alert('new food item:' + text);
   },
+  handleSearch: function(searchText) {
+    this.setState({
+      searchText: searchText
+    })
+  },
   render: function () {
     var {items} = this.state;
     return (
       <div>
+        <FoodSearch onSearc={this.handleSearch}/>
         <FoodList items={items}/>
         <PersonalControls onAddItem={this.handleAddItem}/>
       </div>
